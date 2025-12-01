21466e323e78e17ce8eab20d8ef722a492dfae98
David Capalbo <david.capalbo@uni-konstanz.de>
2025-12-01 16:06:44 +0100

    update src - [external, models]
    support material specific densities for in sessa and ter_sl

--- src/layermodels/models/layer.py
+++ src/layermodels/models/layer.py
@@ -21,9 +21,35 @@ class LayerArgs:
     x3: Maybe[float] = Nothing  # composition fraction of code3
     sigma: Maybe[float] = Nothing  # rms roughness in Å
     tr: Maybe[float] = Nothing  # transition layer thickness in Å
-    rho: Maybe[float] = Nothing  # material density in g/cm3
     chi0: Maybe[float] = Nothing  # layer x-ray susceptibility
     deb_wall: Maybe[float] = Nothing  # Debye-Waller factor
+
+    def __init__(
+        self,
+        codes: Optional[tuple[str, ...]] = None,
+        composition: Optional[tuple[float, ...]] = None,
+        sigma: Optional[float] = None,
+        tr: Optional[float] = None,
+        chi0: Optional[float] = None,
+        deb_wall: Optional[float] = None,
+    ):
+        if codes is not None:
+            self.code1, self.code2, self.code3, self.code4 = (
+                Some(codes[0]) if len(codes) > 0 else Nothing,
+                Some(codes[1]) if len(codes) > 1 else Nothing,
+                Some(codes[2]) if len(codes) > 2 else Nothing,
+                Some(codes[3]) if len(codes) > 3 else Nothing,
+            )
+        if composition is not None:
+            self.x1, self.x2, self.x3 = (
+                Some(composition[0]) if len(composition) > 0 else Nothing,
+                Some(composition[1]) if len(composition) > 1 else Nothing,
+                Some(composition[2]) if len(composition) > 2 else Nothing,
+            )
+        self.sigma = Some(sigma) if sigma is not None else Nothing
+        self.tr = Some(tr) if tr is not None else Nothing
+        self.chi0 = Some(chi0) if chi0 is not None else Nothing
+        self.deb_wall = Some(deb_wall) if deb_wall is not None else Nothing
 
     def __repr__(self) -> str:
         if self == LayerArgs():
@@ -35,8 +61,23 @@ class LayerArgs:
         ArgsErr = Err.TypeErr("LayerArgs")
         if self.sigma != Nothing and self.tr != Nothing:
             return Failure(ArgsErr("Both tr and sigma are defined (mutually exclusive)"))
+        if self.sigma != Nothing and self.sigma.unwrap() > 5:
+            return Success(
+                Some("Warning: High roughness (sigma > 5 Å) may lead to inaccurate results. Use tr=2*(sigma) instead.")
+            )
+
+        num_codes = len([c for c in [self.code1, self.code2, self.code3, self.code4] if c != Nothing])
+        num_comps = len([x for x in [self.x1, self.x2, self.x3] if x != Nothing])
+        if num_codes != num_comps or num_codes > 1 + num_comps:
+            return Failure(
+                ArgsErr(
+                    "The number of defined codes and compositional fractions do not match. Check that for N defined codes, at most N-1 composition fractions are defined.",
+                )
+            )
+
         if sum(x.unwrap() for x in [self.x1, self.x2, self.x3] if x != Nothing) > 1.0:
             return Failure(ArgsErr("Sum of composition fractions exceeds 1.0"))
+
         if (
             all(x != Nothing for x in [self.code1, self.code2, self.code3, self.code4])  # check all codes defined
             and all(x != Nothing for x in [self.x1, self.x2, self.x3])  # check first three fractions defined
@@ -47,10 +88,7 @@ class LayerArgs:
                     "Sum of composition fractions is exactly 1.0 but code4 is defined => implicit fraction of 0 for code4",
                 )
             )
-        if self.sigma != Nothing and self.sigma.unwrap() > 5:
-            return Success(
-                Some("Warning: High roughness (sigma > 5 Å) may lead to inaccurate results. Use tr=2(sigma) instead.")
-            )
+
         return Success(Nothing)
 
 
